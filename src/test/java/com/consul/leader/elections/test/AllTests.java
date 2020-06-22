8f9efae66fbb01a782559a9048b7e1cd07489d31
mahmoudashraf91 <39341898+mahmoudashraf91@users.noreply.github.com>
2020-06-22 20:59:43 +0200

    fix tests

---
+++ src/test/java/com/consul/leader/elections/test/AllTests.java
@@ -0,0 +1,11 @@
+package com.consul.leader.elections.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({AllCases.class})
+public class AllTests {
+
+}
